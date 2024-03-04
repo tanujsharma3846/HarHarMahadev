0ef9b61db1dfb2063c722ea2a5f464bfd3471c41
Tanuj Sharma <tanujsharma3846@gmail.com>
2024-03-04 18:57:07 +0530

    model train done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ logger.info("welcome to logs , har har mahadev !")
 
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-#from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 #from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
